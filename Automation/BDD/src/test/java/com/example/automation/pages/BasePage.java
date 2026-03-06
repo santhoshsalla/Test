f8e98ad42e415be81b818d7fdbad803f9f680973
santhoshsalla <60867988+santhoshsalla@users.noreply.github.com>
2026-03-06 21:59:34 +0530

    Create Automation/BDD/src/test/java/com/example/automation/pages/BasePage.java

---
+++ Automation/BDD/src/test/java/com/example/automation/pages/BasePage.java
@@ -0,0 +1,34 @@
+package com.example.automation.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+
+    protected BasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    protected WebElement waitVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void click(By locator) {
+        waitClickable(locator).click();
+    }
+
+    protected String getText(By locator) {
+        return waitVisible(locator).getText().trim();
+    }
+}
