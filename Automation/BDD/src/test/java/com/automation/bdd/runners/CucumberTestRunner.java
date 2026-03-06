22abcdf58dcb02f6f16bc2899744dd1a3618f6d9
santhoshsalla <60867988+santhoshsalla@users.noreply.github.com>
2026-03-06 22:14:22 +0530

    Create Automation/BDD/src/test/java/com/automation/bdd/runners/CucumberTestRunner.java

---
+++ Automation/BDD/src/test/java/com/automation/bdd/runners/CucumberTestRunner.java
@@ -0,0 +1,15 @@
+package com.automation.bdd.runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @ignore")
+public class CucumberTestRunner {
+}
