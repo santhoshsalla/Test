377b6f882a81b26b2b08ca2f0d52a4f74945f64a
santhoshsalla <60867988+santhoshsalla@users.noreply.github.com>
2026-03-06 22:14:41 +0530

    Create Automation/BDD/src/test/java/com/automation/bdd/core/BaseTest.java

---
+++ Automation/BDD/src/test/java/com/automation/bdd/core/BaseTest.java
@@ -0,0 +1,16 @@
+package com.automation.bdd.core;
+
+import com.automation.bdd.utils.ConfigReader;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver() {
+        return DriverFactory.getDriver();
+    }
+
+    protected void openBaseUrl() {
+        String baseUrl = ConfigReader.getRequired("baseUrl");
+        driver().get(baseUrl);
+    }
+}
