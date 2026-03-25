9b249c3d09a5d3dc8ea23188a2912aa44c7e1785
santhoshsalla <60867988+santhoshsalla@users.noreply.github.com>
2026-03-25 23:47:15 +0530

    Create src/test/java/com/example/pages/LoginPage.java

---
+++ src/test/java/com/example/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package com.example.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("loginBtn");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void login(String username, String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField)).clear();
+        driver.findElement(usernameField).sendKeys(username);
+        driver.findElement(passwordField).clear();
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+}
